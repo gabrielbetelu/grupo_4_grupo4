b6a14f6b3d62ae2425b2db302fbb327fa0a5a105
cecilia.peruzzi <ceciliaperuzzi2@hotmail.com>
2023-05-18 22:11:41 -0300

    sd

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ console.log ('servidor corriendo en el puerto 3030'))
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/login.html'))
 })
 
-app.get("/registro", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/registro.html'))
-})
