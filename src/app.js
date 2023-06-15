ef0a340c80caddb2bfb29cdf0e073c1884019780
Antonellalmb <3lmbmartinez@gmail.com>
2023-06-15 17:28:58 -0300

    creando JSON users y demas

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mainRouter = require('./routes/mainRouter');
+const methodOverride = require('method-override');
 
 app.use(express.static("./public"));
 
 app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
+
+app.use(methodOverride('_method'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
