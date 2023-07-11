da168187d19396d234f1869fa8449280b416b078
cecilia.peruzzi <ceciliaperuzzi2@hotmail.com>
2023-07-11 16:02:00 -0300

    rutas huespedes

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,7 +6,8 @@ const path = require('path');
 //const logMiddleware = require('../middlewares/logMiddleware');
 // const {body}= require('express-validator');
 const regValidation = require('../middlewares/regValidation');
-
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 
 const multerDiskStorage = multer.diskStorage ({
@@ -26,17 +27,20 @@ const fileUpload = multer ({storage:multerDiskStorage});
 
 
 //RUTAS LOGIN
-router.get('/login', controller.login);
+//router.get('/login', controller.login);
+router.get('/login', guestMiddleware, controller.login);
 router.post('/login',  controller.processLogin);
 
 //RUTAS REGISTRO DE USUARIO
 
-router.get('/registro', controller.registro);
+//router.get('/registro', controller.registro);
+router.get('/registro', guestMiddleware, controller.registro);
 router.post('/registro', fileUpload.single ('imagenUsuario'), controller.processRegister);
 router.post('/registro', regValidation ,controller.processRegister);
  
 //RUTA PERFIL DE USUARIO
-router.get('/perfil', controller.perfil);
+//router.get('/perfil', controller.perfil);
+router.get('/perfil', authMiddleware, controller.perfil);
 
 //RUTA ADMINISTRADOR DE PERFILES
 
