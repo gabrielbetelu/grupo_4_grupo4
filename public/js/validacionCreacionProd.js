6d911f74de276c09647bd7651016a949df221c97
cecilia.peruzzi <ceciliaperuzzi2@hotmail.com>
2023-09-01 15:02:57 -0300

    validacion creacion de productos

--- public/js/validacionCreacionProd.js
+++ public/js/validacionCreacionProd.js
@@ -1,43 +1,35 @@
 window.addEventListener("load", () =>{
     const creacionProducto = document.getElementById("creacionValid");
     const fileInput = document.getElementById("imag-valid");
+    
     fileInput.addEventListener('change', function (event){
+    const files = Array.from(event.target.files);
+        let errores = [];
+        for (let i = 0; i< files.length; i++){
+            if(!files[i].type.includes('img')) {
+                errores.push("Imagen invalida")
+              }
+              console.log(errores);      
+        }},
+   
     creacionProducto.addEventListener("submit", function (e){
-        e.preventDefault();
-       
-
-    let errores = [];
-
+    e.preventDefault();
     const nombreProducto = document.getElementById("nomb-valid");
+    const descripProducto = document.getElementById("descr-valid");
+    
+        let errores = [];
+
         if(nombreProducto.value == ""){
             errores.push("Este campo es obligatorio")
         }else if (nombreProducto.value.length < 5){ 
             errores.push("Este campo debe tener al menos 5 caracteres")
         }
-            
-        
-    const descripProducto = document.getElementById("descr-valid");
+    
         if(descripProducto.value.length < 20){
         errores.push("Este campo debe tener al menos 20 caracteres")
         }
     
-        
-        let error = 1;
-        const files = Array.from(event.target.files);
-        console.log(files)
-        //files.map(file => (file.type.includes('image')) ? errores.push("Imagen no valida") : error=0)
-        for (let i = 0; i< files.length; i++){
-            if (files[i].type.includes('image')){
-                error=0;
-
-            }
-            else {
-                errores.push("Imagen no valida")
-            } 
-        }
-        
-      
-        
+         
         console.log(errores);
         if (errores.length == 0){
 
@@ -50,5 +42,5 @@ window.addEventListener("load", () =>{
         }
     })
     
-})
+)
  })       
