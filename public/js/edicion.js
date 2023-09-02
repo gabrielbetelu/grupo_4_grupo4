a68ebe330ce2f67c9afd4ce71fc25c0236738fc8
lvirginia <vir.flopy@gmail.com>
2023-09-02 19:31:19 -0300

    error en la vista

--- public/js/edicion.js
+++ public/js/edicion.js
@@ -1,35 +1,42 @@
 window.addEventListener("load", () =>{
     const formEdicion = document.getElementById("editForm")
-    const erroresLista = document.querySelector(".erroresLista")
+    const imgEdit = document.querySelector(".imgEdit")
 
 
     formEdicion.addEventListener("submit", (e) =>{
         
         e.preventDefault();
 
-        let errores = [];
-
         const nameProd = document.querySelector(".nombre");
+        const descProd = document.querySelector(".descripcion");
+        const errorNom = document.getElementById("errorNom")
+        const errorDesc = document.getElementById("errorDesc")
+        const erroresLista = document.getElementById("erroresLista")
+
+        let errores = [];
+    
         if (nameProd.value == "") {
             errores.push("Este campo es obligatorio");
         }else if (nameProd.value.length < 5){
             errores.push("Este campo debe de tener al menos 5 caracteres");
+            errorNom.innerText = "Este campo debe de tener al menos 5 caracteres"
         }
-
-        const descProd = document.querySelector(".descripcion");
+        
         if (descProd.value.length < 20) {
             errores.push("Este campo debe de tener al menos 20 caracteres");
+            errorDesc.innerText = "Este campo debe de tener al menos 20 caracteres"
+
         }
 
-
+        
         if (errores.length > 0) {
-            erroresLista.innerHTML = ``;
-            for (let error of errores){
-                erroresLista.innerHTML += `<li>${error}</li>`
-            }
+            //erroresLista.innerHTML = ``;
+           // for (let error of errores){
+            //   erroresLista.innerHTML += `<li>${error}</li>`
+            //}
         }else {
-            erroresLista.innerHTML = ``;
-
+            //erroresLista.innerHTML = ``;
+            
             formEdicion.submit();
         }
     })
