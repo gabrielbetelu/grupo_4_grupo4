97aac65d88f69f8cb81a8a328ea071303924b64c
cecilia.peruzzi <ceciliaperuzzi2@hotmail.com>
2023-09-02 23:37:50 -0300

    vald

--- public/js/validacionCreacionProd.js
+++ public/js/validacionCreacionProd.js
@@ -7,9 +7,9 @@ window.addEventListener("load", () =>{
     const files = Array.from(event.target.files);
         let errores = [];
         for (let i = 0; i< files.length; i++){
-            if(!files[i].type.includes('img')) {
+            if(!files[i].type.includes('image')) {
             errores.push("Imagen invalida")
-            errorImagen.innerText= "Debe seleccionar una imagen válida";
+            errorImagen.innerText= "Debe seleccionar una imagen válida"
             }
             console.log(errores);      
         }},
@@ -20,20 +20,19 @@ window.addEventListener("load", () =>{
     const descripProducto = document.getElementById("descr-valid");
     const errorNombre = document.getElementById("errorNombre");
     const errorDescripcion = document.getElementById("errorDescr");
-    console.log(errorNombre)
         let errores = [];
 
         if(nombreProducto.value == ""){
             errores.push("Este campo es obligatorio")
-            errorNombre.innerText = "Campo obligatorio";
+            errorNombre.innerText = "Campo obligatorio"
         }else if (nombreProducto.value.length < 5){ 
             errores.push("Este campo debe tener al menos 5 caracteres")
-            errorNombre.innerText= "Campo obligatorio, mínimo 5 caracteres";
+            errorNombre.innerText= "Campo obligatorio, mínimo 5 caracteres"
         }
     
         if(descripProducto.value.length < 20){
             errores.push("Este campo debe tener al menos 20 caracteres")
-            errorDescripcion.innerText= "Campo obligatorio, mínimo 20 caracteres";
+            errorDescripcion.innerText= "Campo obligatorio, mínimo 20 caracteres"
         }
     
          
