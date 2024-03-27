a28882ab5c685cf4acf23ed47cde3e98f6a393fd
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2024-03-27 13:28:44 -0300

    corrijo variables de producción de config.js en database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,10 +17,10 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
